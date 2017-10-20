0661e6bc430d9f021a459d5e0fb429880ee78a48
eferreyrasancor <elvio.ferreyra@sancorsalud.com.ar>
2017-10-20 08:05:42 -0300

    .

--- src/main/java/com/mavha/cursos/java/app/jpa/dao/EmpleadoDaoJPA.java
+++ src/main/java/com/mavha/cursos/java/app/jpa/dao/EmpleadoDaoJPA.java
@@ -16,48 +16,55 @@ import javax.persistence.Query;
  *
  * @author mdominguez
  */
-public class EmpleadoDaoJPA implements EmpleadoDao{
+public class EmpleadoDaoJPA implements EmpleadoDao {
 
     @PersistenceContext(unitName = "lab01")
     private EntityManager em;
-    
+
     @Override
     public Empleado guardar(Empleado e) {
-      //TODO 2.1 implementar el guardado
-      em.persist(e);
-      em.flush();
-      return e;
+        //TODO 2.1 implementar el guardado
+        em = ConexionJPA.getInstance().em();
+        em.getTransaction().begin();
+        Empleado empGuardado = em.merge(e);
+        em.getTransaction().commit();
+        em.close();
+        return empGuardado;
     }
 
     @Override
     public void borrar(Integer e) {
-      //TODO 2.2 implementar el guardado
-      em.remove(e);
+        //TODO 2.2 implementar el guardado
+        em = ConexionJPA.getInstance().em();
+        em.getTransaction().begin();
+        em.remove(e);
+        em.getTransaction().commit();
+        em.close();
     }
-    
+
     @Override
     public Empleado actualizar(Empleado e) {
         em = ConexionJPA.getInstance().em();
         em.getTransaction().begin();
         Empleado empActualizado = em.merge(e);
         em.getTransaction().commit();
-        em.close();        
+        em.close();
         return empActualizado;
     }
-    
+
     @Override
     public Empleado buscarPorId(Integer id) {
         em = ConexionJPA.getInstance().em();
         em.getTransaction().begin();
         Empleado e = em.find(Empleado.class, id);
         em.getTransaction().commit();
-        em.close();        
+        em.close();
         return e;
     }
 
     @Override
     public List<Empleado> buscarTodos() {
-   String consulta ="SELECT e FROM Empleado e";
+        String consulta = "SELECT e FROM Empleado e";
         em = ConexionJPA.getInstance().em();
         em.getTransaction().begin();
         Query query = em.createQuery(consulta);
@@ -66,24 +73,33 @@ public class EmpleadoDaoJPA implements EmpleadoDao{
         em.close();
         return resultado;
     }
-   
+
     @Override
-    public Double salarioPromedioTodos(){
+    public Double salarioPromedioTodos() {
         ////TODO 2.3 Ejecutar una consulta para conocer el salario promedio de todos los empleados de una empresa
-        return em.createQuery("SELECT AVG(e.salarioHora) FROM Empleado e", Double.class).getSingleResult();
+        em = ConexionJPA.getInstance().em();
+        em.getTransaction().begin();
+        Query query = em.createQuery("SELECT AVG(e.salarioHora) FROM Empleado e", Double.class);
+        Double res = (Double) query.getSingleResult();
+        em.getTransaction().commit();
+        em.close();
+        return res;
     }
 
     @Override
     public List<Tarea> tareasPendientes(Integer idEmpleado) {
         //TODO 2.4 ejecutar una consulta que retorne todas las tareas que tiene pendiente un Empleado. 
         // Las tareas pendientes son todas las tareas asignadas al empleado, que tiene fecha de fin NULL
-        return em.createQuery("SELECT t FROM Tarea t "
+        em = ConexionJPA.getInstance().em();
+        em.getTransaction().begin();
+        Query query = em.createQuery("SELECT t FROM Tarea t "
                 + "WHERE t.fechaFin IS NULL "
-                + "AND t.responsable.id = :empleadoId", Tarea.class)
-                .setParameter("empleadoId", idEmpleado)
-                .getResultList();
+                + "AND t.responsable.id = :empleadoId", Tarea.class);
+        query.setParameter("empleadoId", idEmpleado);
+        List<Tarea> res = query.getResultList();
+        em.getTransaction().commit();
+        em.close();
+        return res;
     }
 
-    
-    
 }
